53a854189a72b3ee6ebfb9da095a4268a1b3cacd
Artur Gasparyan <gasparyanartur99@gmail.com>
2025-07-04 11:19:02 +0200

    fix: give permission to sync image

--- scripts/container/sync_image.sh
+++ scripts/container/sync_image.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-image_path=${APPTAINER_IMAGE_PATH:-/home/x_artga/projdir/images/brain_2025_07_03.sif}
+image_path=${APPTAINER_IMAGE}
+if [ -z "$image_path" ]; then
+    echo "APPTAINER_IMAGE is not set"
+    exit 1
+fi
+
 dest_dir=${APPTAINER_IMAGE_DEST_DIR:-/home/x_artga/projdir/images}
 
 image_name=$(basename $image_path)
-final_image_path="${dest_dir}/${image_name}"
+cluster_url=${CLUSTER_URL:-x_artga@berzelius.nsc.liu.se}
+final_image_path="${cluster_url}:${dest_dir}/${image_name}"
 
-rsync -avz --progress --partial $image_path $final_image_path
+echo "Syncing image $image_path to $final_image_path"
+rsync -avzh --progress --partial $image_path $final_image_path
