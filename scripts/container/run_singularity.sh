defa5f4f8b2df12476e00892a7779ce44a58d436
gasparyanartur <gasparyanartur99@gmail.com>
2025-07-04 13:54:59 +0200

    fix: expand shell

--- scripts/container/run_singularity.sh
+++ scripts/container/run_singularity.sh
@@ -2,4 +2,10 @@
 
 image_path=${APPTAINER_IMAGE_PATH:-/home/x_artga/projdir/images/brain_2025_07_04_10_44_01.sif}
 
-apptainer shell --nv --home /workspace --bind $PWD:/workspace --workdir /workspace --pwd /workspace $image_path
+apptainer shell \
+--nv \
+--home /workspace \
+--bind $PWD:/workspace \
+--workdir /workspace \
+--pwd /workspace \
+$image_path "$@"
