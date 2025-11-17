0196b7916fb72ec7e1af3df279b23870a22aeab5
Artur Gasparyan <gasparyanartur99@gmail.com>
2025-11-17 16:39:08 +0100

    feat: fix stalk

--- scripts/slurm/stalk.sh
+++ scripts/slurm/stalk.sh
@@ -6,8 +6,8 @@ logs_id=$1
 p=$(find $base_log_dir -name *$logs_id*)
 echo $p
 
-if [ $mode == "vim" ]; then
+if [ $mode = "vim" ]; then
     vim $p
-elif [ $mode == "tail" ]; then
+elif [ $mode = "tail" ]; then
     tail -f $p
 fi
