1a7da8061115deb4c70ec8ced547808d4681db97
gasparyanartur <gasparyanartur99@gmail.com>
2026-01-15 16:23:27 +0100

    refactor: clean up alljoined stim order

--- scripts/data/alljoined_16m/download.py
+++ scripts/data/alljoined_16m/download.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import argparse
 import logging
+from typing import cast
 import zipfile
 
 import huggingface_hub
@@ -45,27 +46,31 @@ def main(args):
 
     if "eeg" in args["download_types"]:
         logging.info("Downloading EEG data...")
-        raw_eeg_path = data_path / args["raw_eeg_dir"]
 
-        raw_eeg_path.mkdir(parents=True, exist_ok=True)
         for sub in args["subs"]:
             logging.info(f"Downloading sub-{sub:02}...")
-            huggingface_hub.snapshot_download("Alljoined/Alljoined-1.6M", allow_patterns=f"raw_eeg/sub-{sub:02}/*", repo_type="dataset", local_dir=raw_eeg_path)
+            huggingface_hub.snapshot_download("Alljoined/Alljoined-1.6M", allow_patterns=f"raw_eeg/sub-{sub:02}/*", repo_type="dataset", local_dir=data_path)
 
     if "stim-order" in args["download_types"] or "eeg" in args["download_types"]:
         logging.info("Downloading stim-order files...")
         # The stim-order file is not downloaded with the EEG data, so we need to download it separately
         
-        stim_path = data_path / args["stim_dir"]
-        stim_path.mkdir(parents=True, exist_ok=True)
-        for sub in args["subs"]:
-            huggingface_hub.snapshot_download("Alljoined/Alljoined-1.6M", allow_patterns=f"*{sub:02}*/stim_order.parquet", repo_type="dataset", local_dir=stim_path, revision=STIM_ORDER_REVISION)
-    
-            # Move the stim_order.parquet file to the correct location
-            _found_path = list(stim_path.rglob(f"*/sub-{sub:02}*/stim_order.parquet"))
-            assert len(_found_path) == 1, f"Expected exactly one stim_order.parquet file for sub-{sub:02}, found {len(_found_path)}"
-            
-            _found_path[0].rename(data_path / args["raw_eeg_dir"] / f"sub-{sub:02}" / "stim_order.parquet")
+        #stim_path = data_path / args["stim_dir"]
+        #stim_path.mkdir(parents=True, exist_ok=True)
+
+        with tempfile.TemporaryDirectory(dir=data_path) as tmp_dir:
+            stim_path = Path(tmp_dir)
+
+            for sub in args["subs"]:
+                huggingface_hub.snapshot_download("Alljoined/Alljoined-1.6M", allow_patterns=f"*{sub:02}*/stim_order.parquet", repo_type="dataset", local_dir=stim_path, revision=STIM_ORDER_REVISION)
+        
+                # Move the stim_order.parquet file to the correct location
+                _found_path = list(stim_path.rglob(f"*/sub-{sub:02}*/stim_order.parquet"))
+                assert len(_found_path) == 1, f"Expected exactly one stim_order.parquet file for sub-{sub:02}, found {len(_found_path)}"
+
+                _found_path = cast(Path, _found_path[0])
+                _found_path.rename(data_path / args["raw_eeg_dir"] / f"sub-{sub:02}" / "stim_order.parquet")
+                
         
 
 
