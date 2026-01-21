d9f75f92c5b91724d5aa7d190bd6f75d682b9d26
Artur Gasparyan <gasparyanartur99@gmail.com>
2026-01-21 16:54:38 +0100

    fix: update slurm params

--- scripts/slurm/run_prepare_data_eeg.sh
+++ scripts/slurm/run_prepare_data_eeg.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=prepare_data
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
-#SBATCH --mem=128G
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=64G
 #SBATCH --partition=berzelius-cpu
-#SBATCH --time=1-00:00:00
+#SBATCH --time=04:00:00
 #SBATCH --output=logs/slurm/prepare_data/%j.out
 #SBATCH --account=Berzelius-2025-278
 
